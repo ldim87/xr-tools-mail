2e95889af577b79e9b09389c07a4310c0d46b5f9
Admin <support@1000.menu>
2020-04-03 21:05:40 +0200

    Minor patch - tabs - minor fixes

--- src/Mail.php
+++ src/Mail.php
@@ -7,41 +7,43 @@
 
 namespace XrTools;
 
+use \PHPMailer;
+
 class Mail
 {
-    protected $dbg;
-    
-    protected $config = [];
+	protected $dbg;
+	
+	protected $config = [];
 
-    function __construct(
-        \XrTools\Utils\DebugMessages $dbg,
-        array $config = []
-    ){
-        $this->dbg = $dbg;
-        $this->config = $config;
-    }
+	function __construct(
+		\XrTools\Utils\DebugMessages $dbg,
+		array $config = []
+	){
+		$this->dbg = $dbg;
+		$this->config = $config;
+	}
 
-    /**
-     * Send mail to admin
-     * @param  string $subject Subject
-     * @param  string $message Message body
-     * @return boolean         Mail status
-     */
-    function sendToAdmin($subject, $message){
-        return mail(
-            $this->config['admin_mail_to'] ?? 'admin@localhost',
-            $subject,
-            $message,
-            'From: ' . ($this->config['admin_mail_from'] ?? 'admin@localhost')
-        );
-    }
+	/**
+	 * Send mail to admin
+	 * @param  string $subject Subject
+	 * @param  string $message Message body
+	 * @return boolean         Mail status
+	 */
+	function sendToAdmin($subject, $message){
+		return mail(
+			$this->config['admin_mail_to'] ?? 'admin@localhost',
+			$subject,
+			$message,
+			'From: ' . ($this->config['admin_mail_from'] ?? 'admin@localhost')
+		);
+	}
 	
 	/**
 	 * Send mail to user (by smtp)
 	 * @param string $to      Recipient e-mail address
 	 * @param string $name    Mail subject
 	 * @param string $info    Mail body text
-	 * @param array  $opts     Settings with options:
+	 * @param array  $sys     Settings with options:
 	 *                        <ul>
 	 *                        <li> <strong> debug </strong> boolean
 	 *                        - Debug mode (log errors). Default: false
@@ -50,9 +52,9 @@ class Mail
 	 *                        <li> <strong> from_mail </strong> string
 	 *                        - FROM mail. Default: "info@{$_SERVER['SERVER_NAME']}"
 	 *                        <li> <strong> reply_to </strong> string
-	 *                        - Reply-To header. Default: $opts['from_mail']
+	 *                        - Reply-To header. Default: $sys['from_mail']
 	 *                        <li> <strong> return_path </strong> string
-	 *                        - Return-Path header. Default: $opts['from_mail']
+	 *                        - Return-Path header. Default: $sys['from_mail']
 	 *                        <li> <strong> html </strong> boolean
 	 *                        - Send e-mail in HTML format. Default: false (plain text)
 	 *                        <li> <strong> returnResultObject </strong> bool
@@ -60,129 +62,129 @@ class Mail
 	 *                        </ul>
 	 * @return bool|array    Result status | ['status'=>..., 'message'=>...]
 	 */
-    function send($to, $name, $info, $opts = array()){
-		$opts = $this->prepareOptions($opts);
+	function send(string $to, string $name, string $info, array $sys = []){
 		
-		// Need $to, $name, $info to send mail
-    	if(empty($to) || empty($name) || empty($info)){
-            if($opts['debug']){
-                $this->dbg->log('Mandatory param is missing: 1:' . !empty($to) . ', 2:' . !empty($name) . ', 3:' . !empty($info), __FUNCTION__);
-            }
-            return false;
-        }
-    	
-		$status  = false;
-		$message = '';
-  
-		try{
-			$mail = new \PHPMailer;
-		
+		$debug = !empty($sys['debug']);
+
+		if(empty($to) || empty($name) || empty($info)){
+			
+			if($debug){
+				$missing = [];
+				if(empty($to))   $missing[] = 'to';
+				if(empty($name)) $missing[] = 'name';
+				if(empty($info)) $missing[] = 'info';
+
+				$this->dbg->log('Mandatory input param empty: '.implode(', ', $missing), __METHOD__);
+			}
+
+			return false;
+		}
+
+		if(empty($sys['from_name'])){
+			$sys['from_name'] = $this->config['from_name_default'] ?? 'Noreply';
+		}
+		if(empty($sys['from_mail'])){
+			$sys['from_mail'] = $this->config['from_mail_default'] ?? 'noreply@localhost';
+		}
+		if(empty($sys['reply_to'])){
+			$sys['reply_to'] = $sys['from_mail'];
+		}
+		if(empty($sys['return_path'])){
+			$sys['return_path'] = $sys['from_mail'];
+		}
+		if(empty($sys['sender'])){
+			$sys['sender'] = $sys['return_path'];
+		}
+		if(!isset($sys['html'])){
+			$sys['html'] = false;
+		}
+
+		try {
+			$mail = new PHPMailer;
+
 			$mail->CharSet = 'UTF-8';
-			$mail->From    = $opts['from_mail'];
-			$mail->addReplyTo($opts['reply_to']);
-		
+			$mail->From = $sys['from_mail'];
+			$mail->addReplyTo($sys['reply_to']);
+
 			// bounce
-			$mail->ReturnPath = $opts['return_path'];
-			$mail->Sender     = $opts['sender'];
-		
-			$mail->FromName = $opts['from_name'];
+			$mail->ReturnPath = $sys['return_path'];
+			$mail->Sender = $sys['sender'];
+
+			$mail->FromName = $sys['from_name'];
 			$mail->addAddress($to);
-		
-			if($opts['html']){
+
+			if($sys['html']){
 				$mail->WordWrap = 50;
 				$mail->isHTML(true);
 			}
-		
-			if( !empty($opts['bulk_unsubscribe'])){
+
+			if(!empty($sys['bulk_unsubscribe'])){
 				$mail->addCustomHeader('Precedence', 'bulk');
-				$mail->addCustomHeader('List-Unsubscribe', $opts['bulk_unsubscribe']);
+				$mail->addCustomHeader('List-Unsubscribe', $sys['bulk_unsubscribe']);
 			}
-		
+
 			$mail->Subject = $name;
-			$mail->Body    = $info;
-		
-			// $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-		
+			$mail->Body = $info;
+
+			if(!empty($sys['alt_body'])){
+				// This is the body in plain text for non-HTML mail clients
+				$mail->AltBody = $sys['alt_body'];
+			}
+
 			// Use SMTP
 			$mail->IsSMTP();
-		
+
 			$mail->Host = $this->config['smtp_host'] ?? 'localhost';
-		
-			if( !empty($this->config['smtp_auth_user'])){
+
+			if(!empty($this->config['smtp_auth_user'])){
 				$mail->SMTPAuth = true;
-			
+
 				$mail->Username = $this->config['smtp_auth_user'];
 				$mail->Password = $this->config['smtp_auth_password'] ?? '';
-			
+
 				$mail->Port = $this->config['smtp_port'] ?? 587;
-			
+
 				// force TLS
-				$mail->SMTPSecure  = 'tls';
+				$mail->SMTPSecure = 'tls';
 				$mail->SMTPAutoTLS = true;
-			
-			}else{
+
+			} else {
 				$mail->SMTPAuth = false;
-			
+
 				$mail->Port = $this->config['smtp_port'] ?? 25;
-			
+
 				// disable TLS
-				$mail->SMTPSecure  = '';
+				$mail->SMTPSecure = '';
 				$mail->SMTPAutoTLS = false;
 			}
-		
+
 			// enables SMTP debug information (for testing)
-			if($opts['debug']){
-				$mail->SMTPDebug = \SMTP::DEBUG_SERVER;
+			if($debug){
+				$mail->SMTPDebug = 2;
 			}
-		
+
 			if($mail->send()){
-				$status  = true;
+				$status = true;
 				$message = '';
-			
-				if($opts['debug']){
-					$this->dbg->log('Message sent', __FUNCTION__);
-				}
-			}else{
-				$status  = false;
-				$message = $mail->ErrorInfo;
-			
-				if($opts['debug']){
-					$this->dbg->log('Error: ' . $message, __FUNCTION__);
+
+				if($debug){
+					$this->dbg->log('Message sent', __METHOD__);
 				}
 			}
-		
-		}catch(\Exception $e){
-			$message = 'Error class PHPMailer: ' . $e->getMessage();
+			else {
+				$status = false;
+				$message = $mail->ErrorInfo;
+
+				if($debug){
+					$this->dbg->log('Error: ' . $message, __METHOD__);
+				}
+			}
+		}
+		catch(\Exception $e){
+			$status = false;
+			$message = 'PHPMailer error: ' . $e->getMessage();
 		}
 
-        return !empty($opts['returnResultObject']) ? ['status' => $status, 'message' => $message] : $status;
-    }
-	
-	/**
-	 * Prepare options to send mail
-	 * If the parameter is not present, it is taken from the configuration, and if it is not, then by default
-	 * @param array $opts Options:
-	 *                    <ul>
-	 *                    	<li>@var string from_name
-	 *                    	<li>@var string from_mail
-	 *                    	<li>@var string reply_to
-	 *                    	<li>@var string return_path
-	 *                    	<li>@var string sender
-	 *                    	<li>@var bool html
-	 *                    	<li>@var bool debug
-	 *                    </ul>
-	 * @return array
-	 */
-	private function prepareOptions(array $opts = []){
-		$opts['debug'] = !empty($opts['debug']);
-		$opts['html']  = !empty($opts['html']);
-		
-		$opts['from_name']   = $opts['from_name']	?? ( $this->config['from_name']		?? $_SERVER['SERVER_NAME']			);
-		$opts['from_mail']   = $opts['from_mail']	?? ( $this->config['from_mail']		?? "info@{$_SERVER['SERVER_NAME']}"	);
-		$opts['reply_to']    = $opts['reply_to']	?? ( $this->config['reply_to']		?? $opts['from_mail']				);
-		$opts['return_path'] = $opts['return_path']	?? ( $this->config['return_path']	?? $opts['from_mail']				);
-		$opts['sender']      = $opts['sender']		?? ( $this->config['sender']		?? $opts['return_path']				);
-		
-		return $opts;
+		return !empty($sys['err_info']) ? ['status' => $status, 'message' => $message] : $status;
 	}
 }
